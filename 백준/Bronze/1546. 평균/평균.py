f9315ade97aa0c12610b7c30a47f2ebf92420e47
SeoHyun Song <sh2410152@sookmyung.ac.kr>
2026-03-22 23:24:25 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,6 +1,15 @@
-n = input()
-mylist = list(map(int, input().split()))
-mymax = max(mylist)
-sum = sum(mylist)
+n = int(input()) # n을 숫자로 미리 변환
+# input().split()을 써야 공백을 기준으로 숫자를 잘라옵니다.
+scores = list(map(int, input().split())) 
 
-print(sum * 100 / mymax / int(n))
+total_sum = 0
+max_val = 0
+
+for i in scores:
+    total_sum += i # 이미 위에서 int로 바꿨으므로 i만 더하면 됨
+    if max_val < i:
+        max_val = i
+
+# n은 위에서 int로 변환했으므로 그대로 사용
+result = total_sum * 100 / max_val / n
+print(result)
