5763de5e8364c71e1074272ba231520ca1768855
SeoHyun Song <sh2410152@sookmyung.ac.kr>
2026-03-25 17:07:09 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1104 ms, Memory: 106824 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,21 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-# 1. 입력 받기
+# n(배열의 크기), m(합을 구해야 하는 횟수)
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+# 본래 리스트
+table = [list(map(int, input().split())) for _ in range(n)]
+# 구간 합 배열 생성
+dp = [[0] * (n+1) for _ in range(n + 1)]
 
-# 2. 2차원 구간 합 배열(dp) 생성 및 초기화
-dp = [[0] * (n + 1) for _ in range(n + 1)]
-
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        # 1단계 공식 적용
-        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i-1][j-1]
-
-# 3. 쿼리 처리
+# 구간 합 배열 초기화
+for i in range(1, n+1):
+	for j in range(1, n+1):
+		dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + table[i-1][j-1]
+		
+# 쿼리 처리
 for _ in range(m):
-    x1, y1, x2, y2 = map(int, input().split())
-    # 2단계 공식 적용
-    result = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]
-    print(result)
+	x1, y1, x2, y2 = map(int, input().split())
+	result = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]
+	print(result)
